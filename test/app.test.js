5de8f05f42c2866b7da7c820b91661bb3db7b519
EC2 Default User <ec2-user@ip-172-31-20-7.eu-north-1.compute.internal>
2025-01-10 14:18:03 +0000

    Adding test

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('/home/ec2-user/sample-node-project/app.js'); // path to your app.js
+
+describe('GET /', () => {
+  it('should return a 200 status code', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+  });
+});
+
