685a03d39744ff84cf79b9a2fcb4682da1c4d74b
Zara-Gul <zaragul@evolalabs.com>
2025-01-13 16:44:46 +0500

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-//const app = require('/home/ec2-user/sample-node-project'); // path to your app.js
-const app = require('../app');
+const app = require('/home/ec2-user/sample-node-project/app.js'); // path to your app.js
+//const app = require('../app');
 
 describe('GET /', () => {
   it('should return a 200 status code', async () => {
